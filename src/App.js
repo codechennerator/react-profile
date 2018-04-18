3a60b6c5350f4c9da4e5258b1a27812c335abdc5
Nathan Chen <chenl.nathan@gmail.com>
2018-04-18 12:45:08 +0900

    routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
+import { Home, Portfolio } from "./pages";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Router>
+          <div className = "router">
+            <Switch>
+              <Route exact path = "/" component = {Home} />
+              <Route exact path = "/portfolio" component = {Portfolio} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
---
+++ src/pages/index.js
@@ -0,0 +1,2 @@
+export { default as Home } from "./Home/Home.js";
+export { default as Portfolio } from "./Portfolio/Portfolio.js";
