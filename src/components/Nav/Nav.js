90cf94e58c743ec94d21a2070cf6cc131c2f1f42
Nathan Chen <chenl.nathan@gmail.com>
2018-04-20 17:38:22 +0900

    nav grid styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,7 +3,10 @@ import './Nav.css';
 class Nav extends Component{
     render(){
         return(
-            <nav className = "navigation">
+            <nav className = "top">
+                <div className = "header">
+                    <h2>Nathan Chen</h2>
+                </div>
                 <a href = "/" className = "navitem">Home</a>
                 <a href = "/portfolio" className = "navitem">Portfolio</a>
             </nav>
