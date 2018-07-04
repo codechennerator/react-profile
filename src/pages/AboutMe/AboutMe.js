477fd846c211c896492f38831eb17e881faa4abf
Nathan Chen <chenl.nathan@gmail.com>
2018-07-04 12:46:13 -0700

    updating aboue me page

--- src/pages/AboutMe/AboutMe.js
+++ src/pages/AboutMe/AboutMe.js
@@ -11,13 +11,13 @@ class AboutMe extends Component{
         <div>
             <h1>About Me</h1>
             <MeMain>
-                I'm a full stack developer currently looking for a junior developer position. My technical expertise currently lies in React/Javascript, Node.js, HTML, and MongoDB. However, I'm also proficient
+                I'm a full stack developer currently looking for a junior developer position. My technical expertise currently lies in React/Javascript, Node.js, Express, and MongoDB (MERN). However, I'm also proficient
                 at CSS, jQuery and SQL while also being experienced in Java and C++.
             </MeMain>
             <h2>My Mindset:</h2>
             <p>
                 &emsp;&emsp;My primary goal is to produce highly functional and organized software. For web development, this translates to impressive but bug free websites. I understand the importance
-               of DRY code and the benefits it provides for users, developers, and the bottom line. I'm also not afraid to learn new things. The technology for the web stack is developing incredibly quickly, 
+               of understanding the fundamentals of computer science. I'm always learning how to improve my algorithms, understand different data structures, and optimize time/space complexity. The technology for the web stack is also developing incredibly quickly, 
                and I'm always eager to jump in to upgrade my toolset. 
 
             </p>
@@ -25,7 +25,7 @@ class AboutMe extends Component{
             <p>
                 &emsp;&emsp;I graduated from UCLA in 2016 with a Global Studies Major and a Japanese Language Minor. 
                 Soon after, I found work at Gromax Enteprises Corp. Due to its small size, I helped out in many fields including Accounting, Logistics, Shipping, Customer Service, and IT.
-                Through helping IT, I became eager to enter the tech industry. I began to self-teach at home, using courses such as CS106A to familiarize myself with Java. 
+                Through helping IT, I realized that I had passion and potential in the tech industry. I began to self-teach at home, using courses such as CS106A to familiarize myself with Java. 
                 Looking for more practical skills, I entered the UCI Coding Bootcamp, an intensive, full-stack web development course from Sept. 2017 - Apr. 2018. 
                 Here, I honed my abilities to become a web-developer by adapting to many new computer languages and technologies such as Javascript, React, Node.js, SQL, and MongoDB. 
                 Now, having put in the work for code, I'm looking to code for work!
